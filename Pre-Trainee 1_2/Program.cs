efdae7239000dc5ba003266089fc531730199542
Владислав <vladuk2332@gmail.com>
2025-10-05 10:29:51 +0300

    minor changes

--- Pre-Trainee 1_2/Program.cs
+++ Pre-Trainee 1_2/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Data.Common;
+using System.Diagnostics;
 namespace Pre_Trainee_1_2
 {
     internal class Program
@@ -22,13 +23,20 @@ namespace Pre_Trainee_1_2
         }
         async static Task Main(string[] args)
         {
-             ProcessData("data1");//3 секунды
-             ProcessData("data2");//3
-             ProcessData("data3");//3
+            Stopwatch sw=Stopwatch.StartNew();
+            ProcessData("data1");
+            ProcessData("data2");
+            ProcessData("data3");
+            sw.Stop();
+            Console.WriteLine($"Синхронные методы затратили {sw.Elapsed}");
 
+            sw.Restart();
             ProcessDataAsync("File1");
-            ProcessDataAsync("File2");//сразу все за 3
+            ProcessDataAsync("File2");
             await ProcessDataAsync("File3");
+            sw.Stop();
+            Console.WriteLine($"Асинхронные методы затратили {sw.Elapsed}");
+            
         }
     }
 }
